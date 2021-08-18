7eced9086797170a36ab09a976d6f90b8ace1fdc
santanaruben <rubensantana@outlook.com>
2021-08-18 00:24:26 -0400

    primeros ejercicios

--- test/ejercicio-0/BasicToken.spec.js
+++ test/ejercicio-0/BasicToken.spec.js
@@ -2,55 +2,58 @@ const { ethers } = require("hardhat");
 const { expect } = require("chai");
 
 describe("Ejercicio 0", function () {
-    // Cantidad inicial de 1 millón de tokens (1000000 * 10^18)
-    const INITIAL_SUPPLY = ethers.utils.parseUnits('1000000', 'ether');
-    
-    let deployer, usuario, otroUsuario;
+  // Cantidad inicial de 1 millón de tokens (1000000 * 10^18)
+  const INITIAL_SUPPLY = ethers.utils.parseUnits("1000000", "ether");
 
-    beforeEach(async function () {
-        [deployer, usuario, otroUsuario] = await ethers.getSigners();
+  let deployer, usuario, otroUsuario;
 
-        const BasicToken = await ethers.getContractFactory("BasicToken", deployer);
+  beforeEach(async function () {
+    [deployer, usuario, otroUsuario] = await ethers.getSigners();
 
-        this.token = await BasicToken.deploy(INITIAL_SUPPLY);
+    const BasicToken = await ethers.getContractFactory("BasicToken", deployer);
+
+    this.token = await BasicToken.deploy(INITIAL_SUPPLY);
+  });
+
+  describe("Inicialización", function () {
+    it("La cuenta minter es el deployer", async function () {
+      expect(await this.token.minter()).to.eq(deployer.address);
     });
 
-    describe("Inicialización", function () {
-        it.skip('La cuenta minter es el deployer', async function () {
-            // COMPLETAR
-        });
-
-        it('El total supply del token es el esperado', async function () {
-            expect(await this.token.totalSupply()).to.eq(INITIAL_SUPPLY);
-        });
-
-        it.skip('Todo el total supply es asignado al deployer', async function () {
-            // COMPLETAR
-        });
+    it("El total supply del token es el esperado", async function () {
+      expect(await this.token.totalSupply()).to.eq(INITIAL_SUPPLY);
     });
 
-    describe("Transferencias", function() {
-        it('Un usuario sin fondos no puede transferir', async function () {
-            expect(await this.token.balanceOf(usuario.address)).to.eq(0);
-            await expect(
-                this.token.connect(usuario).transfer(otroUsuario.address, 1)
-            ).to.be.reverted;
-        });
+    it("Todo el total supply es asignado al deployer", async function () {
+      expect(await this.token.balanceOf(deployer.address)).to.eq(
+        INITIAL_SUPPLY
+      );
+    });
+  });
 
-        it('Un usuario con fondos puede transferir', async function () {
-            expect(await this.token.balanceOf(deployer.address)).to.be.gt(0);
-            await this.token.transfer(usuario.address, 1);
-            expect(await this.token.balanceOf(usuario.address)).to.eq(1);
-        });
+  describe("Transferencias", function () {
+    it("Un usuario sin fondos no puede transferir", async function () {
+      expect(await this.token.balanceOf(usuario.address)).to.eq(0);
+      await expect(this.token.connect(usuario).transfer(otroUsuario.address, 1))
+        .to.be.reverted;
     });
 
-    describe("Minting", function() {
-        it.skip('Un usuario sin permisos no puede mintear tokens', async function () {
-            // COMPLETAR
-        });
-
-        it.skip('Un usuario con permisos puede mintear tokens', async function () {
-            // COMPLETAR
-        });
+    it("Un usuario con fondos puede transferir", async function () {
+      expect(await this.token.balanceOf(deployer.address)).to.be.gt(0);
+      await this.token.transfer(usuario.address, 1);
+      expect(await this.token.balanceOf(usuario.address)).to.eq(1);
     });
+  });
+
+  describe("Minting", function () {
+    it("Un usuario sin permisos no puede mintear tokens", async function () {
+      await expect(
+        this.token.connect(usuario).mint(otroUsuario.address, 100)
+      ).to.be.revertedWith("No permitido");
+    });
+
+    it("Un usuario con permisos puede mintear tokens", async function () {
+      await this.token.mint(otroUsuario.address, 100);
+    });
+  });
 });
